9e2f344e5e726bb61ed05c7c5f974d1c4b5063fb
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-08-16 10:21:54 -0400

    Ringdebug and utils
    Change-Id: I7144be0052eb7d1900d17b31edbfe48d0498d7db

--- ring-client-uwp/RingDebug.cpp
+++ ring-client-uwp/RingDebug.cpp
@@ -1,7 +1,6 @@
-﻿#pragma once
-/**************************************************************************
+/***************************************************************************
 * Copyright (C) 2016 by Savoir-faire Linux                                *
-* Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+* Author: J�ger Nicolas <nicolas.jager@savoirfairelinux.com>              *
 *                                                                         *
 * This program is free software; you can redistribute it and/or modify    *
 * it under the terms of the GNU General Public License as published by    *
@@ -16,29 +15,44 @@
 * You should have received a copy of the GNU General Public License       *
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
 **************************************************************************/
-#include "RingConsolePanel.g.h"
 
-namespace RingClientUWP
-{
-namespace Views
-{
-public ref class RingConsolePanel sealed
-{
-public:
-    RingConsolePanel();
-    void output(Platform::String^ message);
+/* client */
+#include "pch.h"
 
-private:
-    void sendCommand();
-    void addCommandToHistory();
-    void _btnSendDbgCmd__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-    void _sendDbgCmd__KeyDown(Platform::Object^ sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs^ e);
+#include "RingDebug.h"
 
-    int historyLevel = 0;
-    /* commands already send */
-    Vector<String^> historyCmds;
-    /* command not already send */
-    String^ currentCmd;
-};
-}
+using namespace RingClientUWP;
+
+using namespace Platform;
+using namespace Windows::UI::Core;
+
+void
+RingDebug::print(const std::string& message,
+                 const Type& type)
+{
+    /* get the current time */
+    std::time_t currentTime = std::time(nullptr);
+    char timeBuffer[64];
+    ctime_s(timeBuffer, sizeof timeBuffer, &currentTime);
+
+    /* timestamp */
+    auto messageTimestamped = timeBuffer + message;
+    std::wstring wString = std::wstring(message.begin(), message.end());
+
+    /* set message type. */
+    switch (type) {
+    case Type::ERR:
+        wString = L"(EE) " + wString;
+        break;
+    case Type::WNG:
+        wString = L"(WW) " + wString;
+        break;
+        /*case Type::message:*/
+    }
+
+    /* screen it into VS debug console */
+    OutputDebugString(wString.c_str());
+
+    /* fire the event. */
+    messageToScreen(ref new String(wString.c_str(), wString.length()));
 }
