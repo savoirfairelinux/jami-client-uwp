3d0f9bb1b16437840df04a370def1dfe89dcba6c
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-08-16 10:21:54 -0400

    Ringdebug and utils
    Change-Id: I7144be0052eb7d1900d17b31edbfe48d0498d7db

---
+++ ring-client-uwp/RingDebug.h
@@ -0,0 +1,69 @@
+/***************************************************************************
+* Copyright (C) 2016 by Savoir-faire Linux                                *
+* Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+*                                                                         *
+* This program is free software; you can redistribute it and/or modify    *
+* it under the terms of the GNU General Public License as published by    *
+* the Free Software Foundation; either version 3 of the License, or       *
+* (at your option) any later version.                                     *
+*                                                                         *
+* This program is distributed in the hope that it will be useful,         *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+* GNU General Public License for more details.                            *
+*                                                                         *
+* You should have received a copy of the GNU General Public License       *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+**************************************************************************/
+#pragma once
+
+namespace RingClientUWP
+{
+
+/* forward declaration */
+ref class RingDebug;
+
+/* delegate */
+delegate void debugMessageToScreen(Platform::String^ message);
+
+/* this is how to implement a singleton class*/
+[Windows::Foundation::Metadata::WebHostHidden]
+public ref class RingDebug sealed
+{
+public:
+    /* singleton */
+    static property RingDebug^ instance
+    {
+        RingDebug^ get()
+        {
+            static RingDebug^ instance_ = ref new RingDebug();
+            return instance_;
+        }
+    }
+
+    /* properties */
+
+    /* functions */
+internal:
+    enum class Type { MSG, WNG, ERR };
+    void print(const std::string& message, const Type& type = Type::MSG);
+
+    /* event */
+    event debugMessageToScreen^ messageToScreen;
+
+private:
+    RingDebug() {}; // singleton
+};
+
+#define MSG_(cstr) CoreApplication::MainView->CoreWindow->Dispatcher->RunAsync(CoreDispatcherPriority::Low, \
+ref new DispatchedHandler([=]() { RingDebug::instance->print(cstr); }))
+
+#define WNG_(cstr) CoreApplication::MainView->CoreWindow->Dispatcher->RunAsync(CoreDispatcherPriority::Low, \
+ref new DispatchedHandler([=]() { RingDebug::instance->print(std::string(cstr), RingDebug::Type::WNG); }))
+
+#define ERR_(cstr) CoreApplication::MainView->CoreWindow->Dispatcher->RunAsync(CoreDispatcherPriority::Low, \
+ref new DispatchedHandler([=]() { RingDebug::instance->print(std::string(cstr), RingDebug::Type::ERR); }))
+
+}
+
+}
