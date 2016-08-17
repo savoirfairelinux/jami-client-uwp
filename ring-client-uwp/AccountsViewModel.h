3e498858f0f1de17da9e9181b5c97e54fd9074b8
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-08-17 15:20:58 -0400

    snapshot #2
    Change-Id: Ie5cd1f5a26d53d375174680ebd1672e55c21b934

---
+++ ring-client-uwp/AccountsViewModel.h
@@ -0,0 +1,56 @@
+#pragma once
+/**************************************************************************
+* Copyright (C) 2016 by Savoir-faire Linux                                *
+* Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+*                                                                         *
+* This program is free software; you can redistribute it and/or modify    *
+* it under the terms of the GNU General Public License as published by    *
+* the Free Software Foundation; either version 3 of the License, or       *
+* (at your option) any later version.                                     *
+*                                                                         *
+* This program is distributed in the hope that it will be useful,         *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+* GNU General Public License for more details.                            *
+*                                                                         *
+* You should have received a copy of the GNU General Public License       *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+**************************************************************************/
+using namespace Platform::Collections;
+
+namespace RingClientUWP
+{
+
+namespace ViewModel {
+public ref class AccountsViewModel sealed
+{
+internal:
+    /* singleton */
+    static property AccountsViewModel^ instance
+    {
+        AccountsViewModel^ get()
+        {
+            static AccountsViewModel^ instance_ = ref new AccountsViewModel();
+            return instance_;
+        }
+    }
+
+    /* functions */
+
+    /* properties */
+    property Vector<Account^>^ accountsList
+    {
+        Vector<Account^>^ get()
+        {
+            return accountsList_;
+        }
+    }
+
+    /* events */
+
+private:
+    AccountsViewModel(); // singleton
+    Vector<Account^>^ accountsList_;
+};
+}
+}
---
+++ ring-client-uwp/MainPage.xaml.cpp
@@ -0,0 +1,104 @@
+﻿/**************************************************************************
+* Copyright (C) 2016 by Savoir-faire Linux                                *
+* Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+*                                                                         *
+* This program is free software; you can redistribute it and/or modify    *
+* it under the terms of the GNU General Public License as published by    *
+* the Free Software Foundation; either version 3 of the License, or       *
+* (at your option) any later version.                                     *
+*                                                                         *
+* This program is distributed in the hope that it will be useful,         *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+* GNU General Public License for more details.                            *
+*                                                                         *
+* You should have received a copy of the GNU General Public License       *
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+**************************************************************************/
+#include "pch.h"
+
+#include "ContactsViewModel.h"
+#include "MessageTextPage.xaml.h"
+#include "SmartPanel.xaml.h"
+#include "RingConsolePanel.xaml.h"
+#include "VideoPage.xaml.h"
+#include "WelcomePage.xaml.h"
+
+#include "MainPage.xaml.h"
+
+using namespace RingClientUWP;
+using namespace RingClientUWP::Views;
+using namespace RingClientUWP::ViewModel;
+
+using namespace Platform;
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::ViewManagement;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Core;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Interop;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::Graphics::Display;
+using namespace Windows::System;
+
+MainPage::MainPage()
+{
+    InitializeComponent();
+
+    /* set the title bar */
+    Window::Current->SetTitleBar(_titleBar_);
+
+    /* populate the frames */
+    _welcomeFrame_->Navigate(TypeName(RingClientUWP::Views::WelcomePage::typeid));
+    _smartPanel_->Navigate(TypeName(RingClientUWP::Views::SmartPanel::typeid));
+    _consolePanel_->Navigate(TypeName(RingClientUWP::Views::RingConsolePanel::typeid));
+    _videoFrame_->Navigate(TypeName(RingClientUWP::Views::VideoPage::typeid));
+    _messageTextFrame_->Navigate(TypeName(RingClientUWP::Views::MessageTextPage::typeid));
+
+    /* connect to delegates */
+    ContactsViewModel::instance->newContactSelected += ref new NewContactSelected([&]() {
+        showFrame(_messageTextFrame_);
+    });
+    ContactsViewModel::instance->noContactSelected += ref new NoContactSelected([&]() {
+        showFrame(_welcomeFrame_);
+    });
+}
+
+void
+MainPage::OnKeyDown(KeyRoutedEventArgs^ e)
+{
+    if (e->Key == VirtualKey::F5) {
+        _outerSplitView_->OpenPaneLength = Window::Current->Bounds.Width;
+        _outerSplitView_->IsPaneOpen = !_outerSplitView_->IsPaneOpen;
+    }
+}
+
+void RingClientUWP::MainPage::_toggleSmartBoxButton__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    _innerSplitView_->IsPaneOpen = !_innerSplitView_->IsPaneOpen;
+    SmartPanel::Mode mode = (_innerSplitView_->IsPaneOpen) ? SmartPanel::Mode::Normal : SmartPanel::Mode::Minimized;
+    dynamic_cast<SmartPanel^>(_smartPanel_->Content)->setMode(mode);
+}
+
+void
+RingClientUWP::MainPage::showFrame(Windows::UI::Xaml::Controls::Frame^ frame)
+{
+    _navGrid_->SetRow(_welcomeFrame_, 0);
+    _navGrid_->SetRow(_messageTextFrame_, 0);
+    _navGrid_->SetRow(_videoFrame_, 0);
+
+    if (frame == _welcomeFrame_) {
+        _navGrid_->SetRow(_welcomeFrame_, 1);
+    } else if (frame == _videoFrame_) {
+        _navGrid_->SetRow(_videoFrame_, 1);
+    } else if (frame == _messageTextFrame_) {
+        _navGrid_->SetRow(_messageTextFrame_, 1);
+        dynamic_cast<MessageTextPage^>(_messageTextFrame_->Content)->updatePageContent();
+    }
+}
