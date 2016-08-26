e4aa77cfe08ed03512266a90d7d31d3cb6990b06
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-08-26 13:00:27 -0400

    message text : add new contact to contacts list when a message arrived from a new contact.
    Change-Id: I82545d852c7d880e2ee1fd5bc479765091b6a830
    Tuleap: #941

--- RingD.cpp
+++ RingD.cpp
@@ -1,4 +1,4 @@
-/***************************************************************************
+/**************************************************************************
 * Copyright (C) 2016 by Savoir-faire Linux                                *
 * Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
 * Author: Traczyk Andreas <traczyk.andreas@savoirfairelinux.com>          *
@@ -103,7 +103,7 @@ RingClientUWP::RingD::startDaemon()
                 stateChange(callId2, state2, code);
 
             }),
-            DRing::exportable_callback<DRing::ConfigurationSignal::IncomingAccountMessage>([this](
+            DRing::exportable_callback<DRing::ConfigurationSignal::IncomingAccountMessage>([&](
                 const std::string& accountId,
                 const std::string& from,
                 const std::map<std::string, std::string>& payloads)
@@ -112,9 +112,17 @@ RingClientUWP::RingD::startDaemon()
                 MSG_("accountId = " + accountId);
                 MSG_("from = " + from);
 
+                auto accountId2 = toPlatformString(accountId);
+                auto from2 = toPlatformString(from);
+
                 for (auto i : payloads) {
                     MSG_("payload = " + i.second);
                     auto payload = Utils::toPlatformString(i.second);
+                    CoreApplication::MainView->CoreWindow->Dispatcher->RunAsync(
+                        CoreDispatcherPriority::Low, ref new DispatchedHandler([=]()
+                    {
+                        incomingAccountMessage(accountId2, from2, payload);
+                    }));
                 }
             }),
             DRing::exportable_callback<DRing::ConfigurationSignal::AccountsChanged>([this]()
