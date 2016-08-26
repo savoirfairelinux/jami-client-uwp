4c5f4c1bfb6162c8f155fd6ddc7d3b9722d144d8
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-08-26 13:00:27 -0400

    text message : add new contact to contacts list when a message arrived from a new contact.
    Change-Id: I82545d852c7d880e2ee1fd5bc479765091b6a830
    Tuleap: #941

--- RingD.h
+++ RingD.h
@@ -1,30 +1,59 @@
 /***************************************************************************
-* Copyright (C) 2016 by Savoir-faire Linux                                *
-* Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
-*                                                                         *
-* This program is free software; you can redistribute it and/or modify    *
-* it under the terms of the GNU General Public License as published by    *
-* the Free Software Foundation; either version 3 of the License, or       *
-* (at your option) any later version.                                     *
-*                                                                         *
-* This program is distributed in the hope that it will be useful,         *
-* but WITHOUT ANY WARRANTY; without even the implied warranty of          *
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
-* GNU General Public License for more details.                            *
-*                                                                         *
-* You should have received a copy of the GNU General Public License       *
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
-**************************************************************************/
-#pragma once
+ * Copyright (C) 2016 by Savoir-faire Linux                                *
+ * Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+ * Author: Traczyk Andreas <andreas.traczyk@savoirfairelinux.com>          *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
 
 using namespace concurrency;
 
 namespace RingClientUWP
 {
 
-public ref class RingD sealed
+// TODO :: move the tasks in private, use enum for order
+ref class Task
 {
 public:
+    property String^ order;
+};
+
+ref class MessageText : public Task {
+
+public:
+    property String^ accountId;
+    property String^ to;
+    property String^ payload;
+};
+
+ref class File : public Task {
+
+};
+
+/* delegates */
+delegate void IncomingCall(String^ accountId, String^ callId, String^ from);
+delegate void StateChange(String^ callId, String^ state, int code);
+delegate void MessageSent(String^ accountId, String^ to, String^ payload);
+delegate void IncomingAccountMessage(String^ accountId, String^ from, String^ payload);
+
+public ref class RingD sealed
+{
+
+public:
+    /* functions */
+
+    /* properties */
     static property RingD^ instance
     {
         RingD^ get()
@@ -42,20 +71,29 @@ public:
         }
     }
 
-
-    /* properties */
-
-
-    /* functions */
-
 internal:
+    /* functions */
     void startDaemon();
+    void dequeueTasks();
+
+    void reloadAccountList();
+
+    void sendMessage(String^ accountId, String^ to, String^ payload);
+
+    /* TODO : move members */
     bool hasConfig;
     std::string accountName;
+
+    /* events */
+    event IncomingCall^ incomingCall;
+    event StateChange^ stateChange;
+    event MessageSent^ messageSent;
+    event IncomingAccountMessage^ incomingAccountMessage;
 
 private:
     RingD(); // singleton
     std::string localFolder_;
     bool daemonRunning_ = false;
+    std::queue<Task^> tasksList_;
 };
 }
