5cc12c947460664acab2c561a68c9b8df0cc12a2
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-08-26 13:00:27 -0400

    message text : add new contact to contacts list when a message arrived from a new contact.
    Change-Id: I82545d852c7d880e2ee1fd5bc479765091b6a830
    Tuleap: #941

--- RingD.h
+++ RingD.h
@@ -1,6 +1,8 @@
-/***************************************************************************
+#pragma once
+/**************************************************************************
 * Copyright (C) 2016 by Savoir-faire Linux                                *
 * Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+* Author: Traczyk Andreas <traczyk.andreas@savoirfairelinux.com>          *
 *                                                                         *
 * This program is free software; you can redistribute it and/or modify    *
 * it under the terms of the GNU General Public License as published by    *
@@ -15,7 +17,6 @@
 * You should have received a copy of the GNU General Public License       *
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
 **************************************************************************/
-#pragma once
 
 using namespace concurrency;
 
@@ -25,6 +26,8 @@ namespace RingClientUWP
 /* delegate */
 delegate void IncomingCall(String^ accountId, String^ callId, String^ from);
 delegate void StateChange(String^ callId, String^ state, int code);
+delegate void IncomingAccountMessage(String^ accountId, String^ from, String^ payload);
+
 
 public ref class RingD sealed
 {
@@ -60,6 +63,7 @@ internal:
     /* events */
     event IncomingCall^ incomingCall;
     event StateChange^ stateChange;
+    event IncomingAccountMessage^ incomingAccountMessage;
 
 private:
     /* sub classes */
