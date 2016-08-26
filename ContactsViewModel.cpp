c2e098eba4679b53dde29e195f7021681290ebd8
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-08-26 13:00:27 -0400

    message text : add new contact to contacts list when a message arrived from a new contact.
    Change-Id: I82545d852c7d880e2ee1fd5bc479765091b6a830
    Tuleap: #941

--- ContactsViewModel.cpp
+++ ContactsViewModel.cpp
@@ -1,6 +1,7 @@
-/***************************************************************************
+/**************************************************************************
 * Copyright (C) 2016 by Savoir-faire Linux                                *
 * Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+* Author: Traczyk Andreas <traczyk.andreas@savoirfairelinux.com>          *
 *                                                                         *
 * This program is free software; you can redistribute it and/or modify    *
 * it under the terms of the GNU General Public License as published by    *
@@ -18,8 +19,11 @@
 #include "pch.h"
 #include "ContactsViewModel.h"
 
+using namespace Windows::ApplicationModel::Core;
 using namespace Windows::Data::Json;
 using namespace Windows::Storage;
+using namespace Windows::Storage::Streams;
+using namespace Windows::UI::Core;
 
 using namespace RingClientUWP;
 using namespace ViewModel;
@@ -28,10 +32,26 @@ ContactsViewModel::ContactsViewModel()
 {
     contactsList_ = ref new Vector<Contact^>();
     openContactsFromFile();
+
+    /* connect delegates. */
+    RingD::instance->incomingAccountMessage += ref new IncomingAccountMessage([&](String^ accountId,
+    String^ from, String^ payload) {
+        auto contact = findContactByName(from);
+
+        if (contact == nullptr)
+            contact = addNewContact(from, from); // contact checked inside addNewContact.
+
+        if (contact == nullptr) {
+            ERR_("contact not handled!");
+            return;
+        }
+
+    });
+
 }
 
 Contact^
-RingClientUWP::ViewModel::ContactsViewModel::findContactByName(String ^ name)
+ContactsViewModel::findContactByName(String^ name)
 {
     for each (Contact^ contact in contactsList_)
         if (contact->name_ == name)
@@ -41,10 +61,10 @@ RingClientUWP::ViewModel::ContactsViewModel::findContactByName(String ^ name)
 }
 
 Contact^
-RingClientUWP::ViewModel::ContactsViewModel::addNewContact(String^ name, String^ ringId)
+ContactsViewModel::addNewContact(String^ name, String^ ringId)
 {
     if (contactsList_ && !findContactByName(name)) {
-        Contact^ contact = ref new Contact(name, ringId);
+        Contact^ contact = ref new Contact(name, name);
         contactsList_->Append(contact);
         saveContactsToFile();
         return contact;
