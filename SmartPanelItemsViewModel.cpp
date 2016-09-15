40c2f6f96cd266824106b306fb02ce3310d4ecf0
atraczyk <andreastraczyk@gmail.com>
2016-09-15 11:12:16 -0400

    contacts: add a view-model for SmartPanelItem controls
    - adds a view-model to expose SmartPanelItem controls
    - replaces SmartPanelItem control functions for finding SmartPanelItems
      originally defined in SmartPanel
    - adds selection of the corresponding SmartPanelItem when calling,
      and accepting/refusing a call
    
    Change-Id: I635a805a8e8bf6a412a6ca3d8c7ec907d3c16c2b
    Tuleap: #1041

---
+++ SmartPanelItemsViewModel.cpp
@@ -0,0 +1,78 @@
+/***************************************************************************
+ * Copyright (C) 2016 by Savoir-faire Linux                                *
+ * Author: Jäger Nicolas <nicolas.jager@savoirfairelinux.com>              *
+ * Author: Traczyk Andreas <andreas.traczyk@savoirfairelinux.com>          *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify    *
+ * it under the terms of the GNU General Public License as published by    *
+ * the Free Software Foundation; either version 3 of the License, or       *
+ * (at your option) any later version.                                     *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful,         *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License       *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ **************************************************************************/
+#include "pch.h"
+
+#include "SmartPanelItemsViewModel.h"
+
+using namespace Windows::ApplicationModel::Core;
+using namespace Windows::Data::Json;
+using namespace Windows::Storage;
+using namespace Windows::Storage::Streams;
+using namespace Windows::UI::Core;
+
+
+using namespace RingClientUWP;
+using namespace ViewModel;
+
+SmartPanelItemsViewModel::SmartPanelItemsViewModel()
+{
+    itemsList_ = ref new Vector<SmartPanelItem^>();
+}
+
+SmartPanelItem^
+SmartPanelItemsViewModel::findItem(Call^ call)
+{
+    for each (SmartPanelItem^ item in itemsList)
+        if (item->_call == call)
+            return item;
+
+    return nullptr;
+}
+
+SmartPanelItem^
+SmartPanelItemsViewModel::findItem(Contact^ contact)
+{
+    for each (SmartPanelItem^ item in itemsList)
+        if (item->_contact == contact)
+            return item;
+
+    return nullptr;
+}
+
+unsigned int
+SmartPanelItemsViewModel::getIndex(Call^ call)
+{
+    unsigned int i;
+    for (i = 0; i < itemsList_->Size; i++) {
+        if (itemsList_->GetAt(i)->_call == call)
+            break;
+    }
+    return i;
+}
+
+unsigned int
+SmartPanelItemsViewModel::getIndex(Contact^ contact)
+{
+    unsigned int i;
+    for (i = 0; i < itemsList_->Size; i++) {
+        if (itemsList_->GetAt(i)->_contact == contact)
+            break;
+    }
+    return i;
+}
