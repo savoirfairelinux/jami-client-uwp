daa5b6e8f1f2d41f0493acec99423423f650e7bb
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-10-04 08:46:19 -0400

    patch pour amelioration des etats
    Change-Id: I5fd57088cc6a687edbaf80d556d275b7ea1040a7

--- VideoPage.xaml.cpp
+++ VideoPage.xaml.cpp
@@ -203,16 +203,7 @@ void RingClientUWP::Views::VideoPage::_btnCancel__Click(Platform::Object^ sender
 void RingClientUWP::Views::VideoPage::_btnHangUp__Tapped(Platform::Object^ sender, Windows::UI::Xaml::Input::TappedRoutedEventArgs^ e)
 {
     auto item = SmartPanelItemsViewModel::instance->_selectedItem;
-    auto call = item->_call;
-
-    /*if (call)
-        RingD::instance->hangUpCall(call);*/
-
-    if (item->_callId != "") {
-        MSG_("$1 item->callid != vide ");
-        RingD::instance->hangUpCall2(item->_callId);
-        item->_callId = "";
-    }
+    RingD::instance->hangUpCall2(item->_callId);
 
     pressHangUpCall();
 }
