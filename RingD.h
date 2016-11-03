636178d4a107d2d2e103cb0edc84971b9332c80d
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-11-03 11:13:47 -0400

    console : add some commands
    *EXPERIMENTAL* requires much more tests.
    
    Change-Id: Ie00fa5366c886ff1570d6879288fea62b98e823d

--- RingD.h
+++ RingD.h
@@ -101,6 +101,9 @@ internal:
     void updateAccount(String^ accountId);
     void deleteAccount(String^ accountId);
     void registerThisDevice(String^ pin, String^ archivePassword);
+    void getCallsList();
+    void killCall(String^ callId);
+    void switchDebug();
 
     /* TODO : move members */
     String ^ currentCallId; // to save ongoing call id during visibility change
@@ -137,7 +140,10 @@ private:
         DeleteAccount,
         LookUpName,
         LookUpAddress,
-        RegisterName
+        RegisterName,
+        GetCallsList,
+        KillCall,
+        switchDebug,
     };
 
 
@@ -182,5 +188,6 @@ private:
     std::queue<Task^> tasksList_;
     StartingStatus startingStatus_ = StartingStatus::NORMAL;
     bool editModeOn_ = false;
+    bool debugModeOn_ = true;
 };
 }
