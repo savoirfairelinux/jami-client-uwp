25b4a257f1f655204a91e77652c8c432b1d74683
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-11-14 09:32:02 -0500

    wizzard : add ethereum
    Change-Id: I81a790cebc91ed5c27c828fda0f5f6eadbc82c1a

--- Wizard.xaml.h
+++ Wizard.xaml.h
@@ -11,13 +11,33 @@ public ref class Wizard sealed
 {
 public:
     Wizard();
+
+protected:
+    virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+
 private:
     void _createAccountYes__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
     void _showCreateAccountMenuBtn__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
     void _showAddAccountMenuBtn__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
     void _avatarWebcamCaptureBtn__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
     void _addAccountYes__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
-    void validatePassword(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+    void _usernameTextBox__KeyUp(Platform::Object^ sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs^ e);
+    void _RegisterState__Toggled(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+    void _fullnameTextBox__KeyUp(Platform::Object^ sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs^ e);
+    void collapseMenus(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+    void _step1button__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+    void _step2button__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+    void OnregisteredNameFound(LookupStatus status);
+    void _password__PasswordChanged(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+    void _passwordCheck__PasswordChanged(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+    void checkState();
+
+    /*members*/
+    bool isPasswordValid;
+    bool isPasswordsMatching;
+    bool isPublic = true;
+    bool isUsernameValid; // available
+    bool isFullNameValid;
 };
 
 }
