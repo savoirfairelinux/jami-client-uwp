8905b927fe32d92eb01d47c41803118975fe267a
Nicolas Jager <nicolas.jager@savoirfairelinux.com>
2016-11-14 09:32:02 -0500

    wizzard : add ethereum
    Change-Id: I81a790cebc91ed5c27c828fda0f5f6eadbc82c1a

--- Wizard.xaml.cpp
+++ Wizard.xaml.cpp
@@ -4,6 +4,8 @@
 
 #include "MainPage.xaml.h"
 
+#include "gnutls\gnutls.h"
+
 using namespace RingClientUWP::Views;
 
 using namespace Concurrency;
@@ -26,17 +28,28 @@ using namespace Windows::UI::Xaml::Shapes;
 Wizard::Wizard()
 {
     InitializeComponent();
+    /* connect to delegates */
+    RingD::instance->registeredNameFound += ref new RingClientUWP::RegisteredNameFound(this, &RingClientUWP::Views::Wizard::OnregisteredNameFound);
+}
+
+void RingClientUWP::Views::Wizard::OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs ^ e)
+{
+    gnutls_global_init();
+    RingD::instance->registerCallbacks();
+    RingD::instance->initDaemon( DRing::DRING_FLAG_CONSOLE_LOG | DRing::DRING_FLAG_DEBUG );
+    Video::VideoManager::instance->captureManager()->EnumerateWebcamsAsync();
 }
 
 void
 Wizard::_createAccountYes__Click(Object^ sender, RoutedEventArgs^ e)
 {
-    auto alias = _aliasTextBox_->Text;
-    RingD::instance->_startingStatus = StartingStatus::REGISTERING_ON_THIS_PC;
-
+    //RingD::instance->_startingStatus = StartingStatus::REGISTERING_ON_THIS_PC;
     this->Dispatcher->RunAsync(Windows::UI::Core::CoreDispatcherPriority::High, ref new Windows::UI::Core::DispatchedHandler([this] () {
         this->Frame->Navigate(Windows::UI::Xaml::Interop::TypeName(RingClientUWP::MainPage::typeid));
-        RingD::instance->createRINGAccount(_aliasTextBox_->Text, _password_->Password, true);
+        RingD::instance->createRINGAccount(_fullnameTextBox_->Text
+                                           , _password_->Password
+                                           , true // upnp by default set to true
+                                           , (_RegisterState_->IsOn)? _usernameTextBox_->Text : "");
         _password_->Password = "";
     }));
 }
@@ -126,11 +139,172 @@ void RingClientUWP::Views::Wizard::_addAccountYes__Click(Platform::Object^ sende
     }));
 }
 
-
-void RingClientUWP::Views::Wizard::validatePassword(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+void RingClientUWP::Views::Wizard::_password__PasswordChanged(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
-    _createAccountYes_->IsEnabled = (_password_->Password
-                                     == _passwordCheck_->Password
-                                     && _password_->Password->Length() > 0)
-                                    ? true : false;
+    isPasswordValid = (_password_->Password->Length() > 0)
+                      ? true : false;
+
+    if (isPasswordValid) {
+        _createAccountYes_->IsEnabled = true;
+        _passwordValid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+        _passwordInvalid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    } else {
+        _createAccountYes_->IsEnabled = false;
+        _passwordValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _passwordInvalid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+    }
+
+    isPasswordsMatching = (_password_->Password
+                           == _passwordCheck_->Password
+                           && _password_->Password->Length() > 0)
+                          ? true : false;
+
+    if (isPasswordsMatching) {
+        _passwordCheckValid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+        _passwordCheckInvalid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    }
+    else {
+        _passwordCheckValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _passwordCheckInvalid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+    }
+
+    checkState();
 }
+
+void RingClientUWP::Views::Wizard::_passwordCheck__PasswordChanged(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    isPasswordsMatching = (_password_->Password
+                           == _passwordCheck_->Password
+                           && _password_->Password->Length() > 0)
+                          ? true : false;
+
+    if (isPasswordsMatching) {
+        _passwordCheckValid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+        _passwordCheckInvalid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    }
+    else {
+        _passwordCheckValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _passwordCheckInvalid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+    }
+
+    checkState();
+}
+
+void RingClientUWP::Views::Wizard::checkState()
+{
+    if ((isPublic && isPasswordValid && isPasswordsMatching && isUsernameValid && isFullNameValid)
+            ||(!isPublic && isPasswordValid && isPasswordsMatching && isFullNameValid))
+        _createAccountYes_->IsEnabled = true;
+    else
+        _createAccountYes_->IsEnabled = false;
+}
+
+void RingClientUWP::Views::Wizard::_usernameTextBox__KeyUp(Platform::Object^ sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs^ e)
+{
+    auto alias = dynamic_cast<TextBox^>(sender)->Text;
+
+    if (alias->IsEmpty()) {
+        _usernameValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _usernameInvalid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+    } else {
+        RingD::instance->lookUpName(alias);
+        _usernameValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _usernameInvalid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    }
+
+    // checkState(); is made in OnregisteredNameFound
+}
+
+void RingClientUWP::Views::Wizard::_RegisterState__Toggled(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    auto toggleSwitch =  dynamic_cast<ToggleSwitch^>(sender);
+
+    if (_usernameTextBox_ == nullptr) // avoid trouble when InitializeComponent is called for Wizard.
+        return;
+
+    isPublic = toggleSwitch->IsOn;
+
+    if (isPublic) {
+        _usernameTextBox_->IsEnabled = true;
+        _whatWilHappen_->Text = "peoples will find you with your username.";
+    } else {
+        _usernameTextBox_->IsEnabled = false;
+        _whatWilHappen_->Text = "you'll have to send your ringId.";
+    }
+
+    checkState();
+}
+
+void RingClientUWP::Views::Wizard::_fullnameTextBox__KeyUp(Platform::Object^ sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs^ e)
+{
+    isFullNameValid = !_fullnameTextBox_->Text->IsEmpty();
+
+    if (isFullNameValid) {
+        _fullnameValid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+        _fullnameInvalid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    } else {
+        _fullnameValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _fullnameInvalid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+    }
+
+    checkState();
+}
+
+void RingClientUWP::Views::Wizard::collapseMenus(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    _accountAddMenuGrid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    _showAddAccountMenuTitle_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    _showAddAccountMenuBtn_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+
+    _accountCreationMenuGrid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    _showCreateAccountMenuTitle_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    _showCreateAccountMenuBtn_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+}
+
+void RingClientUWP::Views::Wizard::_step1button__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    _step1Menu_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+    _step2Menu_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+
+    _nextstep_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+    _addAccountYes_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+}
+
+void RingClientUWP::Views::Wizard::_step2button__Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+    _step1Menu_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    _step2Menu_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+
+    _nextstep_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+    _addAccountYes_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+}
+
+void RingClientUWP::Views::Wizard::OnregisteredNameFound(LookupStatus status)
+{
+    switch (status)
+    {
+    case LookupStatus::SUCCESS:
+        _usernameValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _usernameInvalid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+        isUsernameValid = false;
+        break;
+    case LookupStatus::INVALID_NAME:
+        _usernameValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _usernameInvalid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+        isUsernameValid = false;
+        break;
+    case LookupStatus::NOT_FOUND:
+        _usernameValid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+        _usernameInvalid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        isUsernameValid = true;
+        break;
+    case LookupStatus::ERRORR:
+        _usernameValid_->Visibility = Windows::UI::Xaml::Visibility::Collapsed;
+        _usernameInvalid_->Visibility = Windows::UI::Xaml::Visibility::Visible;
+        isUsernameValid = false;
+        break;
+    }
+
+    checkState();
+}
+
